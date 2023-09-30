50fda92d44d70493068fed89cf4c1615cd73ced1
MAKARASAMA\Admin <nuolmakara@gmail.com>
2023-09-30 23:36:09 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import PostCard from "./components/PostCard.vue";
+const posts = [
+  {
+    id: 1,
+    img: "https://business-cambodia.com/cms/assets/a8b1dabe-ccc2-4190-b7ce-a96ca2b7fa38",
+    title:
+      "អគ្គនាយកដ្ឋានពន្ធដារឱ្យអាជីវកម្ម ២ ប្រភេទនេះ ត្រូវតែចេញវិក្កយបត្រឱ្យអ្នកទិញ...",
+    tag: "#ចាប់ផ្តើមអាជីវកម្ម",
+  },
+  {
+    id: 2,
+    img: "https://business-cambodia.com/cms/assets/5ce8bc99-664e-4259-ae1c-d8e819f5870b",
+    title:
+      "សមាគមមូលនិធិគ្រួសារខ្មែរ សមាគមសប្បុរសធម៌មេត្តា និងក្រុមហ៊ុន Cambodia My Second Home (CM2H)...",
+    tag: "#អចលនទ្រព្យ",
+  },
+  {
+    id: 3,
+    img: "https://business-cambodia.com/cms/assets/bbb297f5-6f93-45ce-a828-835d38f19af9",
+    title:
+      "អគ្គនាយកដ្ឋានពន្ធដារបញ្ជាក់! អាជីវកម្មដែលរកចំណូលប្រចាំឆ្នាំតិចជាងចំនួននេះ មិនតម្រូវឱ្យចុះបញ្ជីពន្ធដារទេ...",
+    tag: "#ចាប់ផ្តើមអាជីវកម្ម",
+    author: {
+        name: "author1",
+        publishedAt: "20-10-2023",
+        numOfView: "1M"
+    },
+  },
+];
+</script>
+
+<template>
+  <div class="container">
+    <PostCard
+      v-for="post in posts"
+      :img="post.img"
+      :title="post.title"
+      :tag="post.tag"
+    />
+  </div>
+</template>
+
+<style>
+.container {
+  display: flex;
+  gap: 20px;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
---
+++ src/components/PostCard.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+interface Props {
+  img: string;
+  title: string;
+  tag: string;
+}
+
+defineProps<Props>()
+
+// defineProps({
+//   img: Object,
+//   title: String,
+//   tag: String,
+// })
+</script>
+
+<template>
+  <div class="card">
+    <div class="thumbnail">
+      <img :src="img"  />
+    </div>
+    <div class="content">
+      <div class="tag">{{ tag }}</div>
+      <div class="title">{{ title }}</div>
+    </div>
+  </div>
+</template>
+
+<style>
+body {
+    background-color: #ffffff;
+    max-width: 1024px;    
+    margin: 100px auto;
+}
+img {
+  width: 100%;
+  height: 1000%;
+}
+
+.content {
+    padding: 16px;
+}
+
+.card {
+  max-width: 300px;
+  background-color: white;
+  box-shadow: 0 10px 10px #e5e7eb;
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.tag {
+    background-color: #e5e7eb;
+    width: fit-content;
+    padding: 2px 8px;
+    border-radius: 0.25rem;
+}
+
+.title {
+    margin-top: 10px;
+}
+.title:hover {
+    text-decoration: underline;
+    cursor: pointer;    
+}
+
+</style>
